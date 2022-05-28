096ab79a07dbd7ce4e43183365640f54821644c4
Aditya Shukla <shukladitya9@gmail.com>
2022-05-28 13:05:57 +0530

    Level 3 interaction

--- Level 2/index.js
+++ Level 2/index.js
@@ -2,21 +2,27 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - 4;
 
-getRandomColor = () => {
-  var letters = "0123456789ABCDEF"; //hexadecimal
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]; //math.random=>0 to 1 => this into 16 => anyrhing between 0 to 16.
-  }
-  return color;
-};
+let colors = ["#511845", "#900C3F", "#C70039", "#FF5733"];
+
 let c = canvas.getContext("2d");
 
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+document.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
 class Circle {
-  constructor(x, y, radius, color, velocityX, velocityY) {
+  constructor(x, y, minRadius, maxRadius, color, velocityX, velocityY) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+    this.minRadius = minRadius;
+    this.maxRadius = maxRadius;
+    this.tempRadius = minRadius;
     this.color = color;
     this.velocityX = velocityX;
     this.velocityY = velocityY;
@@ -24,68 +30,56 @@ class Circle {
 
   drawCircle = () => {
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.arc(this.x, this.y, this.minRadius, 0, Math.PI * 2, false);
+    c.strokeStyle = this.color;
     c.stroke();
     c.fillStyle = this.color;
     c.fill();
   };
 
-  updatePosition = () => {
+  updatePositionAndRadius = () => {
     this.x += this.velocityX;
     this.y += this.velocityY;
-    if (this.x > innerWidth - this.radius || this.x < 0)
+    if (this.x > innerWidth - this.minRadius || this.x < 0)
       this.velocityX = -this.velocityX;
-    if (this.y > innerHeight - this.radius || this.y < 0)
+    if (this.y > innerHeight - this.minRadius || this.y < 0)
       this.velocityY = -this.velocityY;
-    this.drawCircle();
-  };
 
-  updateSpeed = (condition) => {
-    if (condition == "INC") {
-      this.velocityX <= 0 ? (this.velocityX -= 6) : (this.velocityX += 6);
-      this.velocityY <= 0 ? (this.velocityY -= 6) : (this.velocityY += 6);
-    } else {
-      this.velocityX <= 0 ? (this.velocityX += 6) : (this.velocityX -= 6);
-      this.velocityY <= 0 ? (this.velocityY += 6) : (this.velocityY -= 6);
-    }
+    //interactivity with mouse
+
+    if (Math.abs(this.x - mouse.x) < 50 && Math.abs(this.y - mouse.y) < 50) {
+      if (this.minRadius <= this.maxRadius) this.minRadius += 4;
+    } else if (this.minRadius > this.tempRadius) this.minRadius--;
+    this.drawCircle();
   };
 }
 let circlesArray = [];
 
-document.querySelector(".inc").addEventListener("click", () => {
-  for (i = 0; i < 100; i++) circlesArray[i].updateSpeed("INC");
-});
-
-document.querySelector(".dec").addEventListener("click", () => {
-  for (i = 0; i < 100; i++) circlesArray[i].updateSpeed("DEC");
-});
-
-document.querySelector(".refresh").addEventListener("click", () => {
- location.reload();
-});
-
-for (i = 0; i < 200; i++) {
+for (i = 0; i < 1600; i++) {
   x = Math.floor(Math.random() * innerWidth);
   y = Math.floor(Math.random() * innerHeight);
-  radius = Math.floor(Math.random() * 50);
-  velocityX = Math.floor(Math.random() * 8);
-  velocityY = Math.floor(Math.random() * 8);
+  minRadius = Math.floor(Math.random() * 4) + 1;
+  maxRadius = 50;
+  velocityX = Math.floor(Math.random() * 3) + 1;
+  velocityY = Math.floor(Math.random() * 3) + 1;
 
-  if (x < radius) x += radius;
-  if (x > innerWidth - radius) x -= radius;
-  if (y < radius) y += radius;
-  if (y > innerHeight - radius) y -= radius;
-  color = getRandomColor();
-  circlesArray.push(new Circle(x, y, radius, color, velocityX, velocityY));
+  if (x < minRadius) x += minRadius;
+  if (x > innerWidth - minRadius) x -= minRadius;
+  if (y < minRadius) y += minRadius;
+  if (y > innerHeight - minRadius) y -= minRadius;
+  color = colors[Math.floor(Math.random() * 4)];
+  circlesArray.push(
+    new Circle(x, y, minRadius, maxRadius, color, velocityX, velocityY)
+  );
 }
 
-for (i = 0; i < 200; i++) {
+for (i = 0; i < 1600; i++) {
   circlesArray[i].drawCircle();
 }
 
 let animate = () => {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  for (i = 0; i < 100; i++) circlesArray[i].updatePosition();
+  for (i = 0; i < 1600; i++) circlesArray[i].updatePositionAndRadius();
 };
 animate();
